0a9a12b08ef20a308455a9aa8e17cbfd2f5bcde4
njligames <jamesfolk1@gmail.com>
2017-02-08 11:20:23 -0500

    added an option to enable/disble building the lua and luac executables. if building for tvos, it will remove the system() function call, as it is unsupported on that platform

--- src/loslib.c
+++ src/loslib.c
@@ -129,7 +129,19 @@ static time_t l_checktime (lua_State *L, int arg) {
 
 static int os_execute (lua_State *L) {
   const char *cmd = luaL_optstring(L, 1, NULL);
-  int stat = system(cmd);
+
+  int stat = 0;
+
+#if defined(__APPLE__)
+#include "TargetConditionals.h"
+
+#if (!defined(TARGET_OS_TV) )
+    {
+    stat = system(cmd);
+    }
+#endif
+#endif
+
   if (cmd != NULL)
     return luaL_execresult(L, stat);
   else {
